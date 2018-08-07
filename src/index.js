fc2848c2ccdf7cc61b7ca34a9081448c36f9fb3f
Siri McClean <nmcclean@wellesley.edu>
2018-08-07 16:23:27 -0400

    continuing after MAP API

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
 console.log('this is trip planner1 ')
+
+const mapboxgl = require("mapbox-gl");
+
+mapboxgl.accessToken = 'pk.eyJ1Ijoibm1jY2xlYW4iLCJhIjoiY2prazVkY2l0MGQzZDNxbWd1cGFsNGN2OSJ9.0OhxFhz5mjIARZMf-bb3wA';
+
+const map = new mapboxgl.Map({
+  container: "map",
+  center: [-74.009, 40.705], // FullStack NY coordinates; alternatively, use [-87.6354, 41.8885] for Chicago
+  zoom: 12, // starting zoom
+  style: "mapbox://styles/mapbox/streets-v10" // mapbox has lots of different map styles available.
+});
